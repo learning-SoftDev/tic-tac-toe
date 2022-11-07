021d0cdd475d2317726774d590bd65670c98895c
Mark Ramos <ramosmarkjoshua@gmail.com>
2022-11-08 00:52:43 +0800

    Finished with the initial draft. Will refactor.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const gameboardItems = document.querySelectorAll('.gameboard-items');
 const buttonRestart = document.querySelector('#button-restart');
+const resultID = document.querySelector('#result-id')
 
-let currentTurn = 'O'
+resultID.style.visibility="hidden"
+//Setting global variables
+let currentTurn = 'O';
 let gameboard = ['','','','','','','','',''];
-let winCounterO = 0
-let winCounterX = 0
+let winCounter = 0;
+let PlayerO = [];
+let PlayerX = [];
+let turnCount = 0;
 const winningPatterns = [
     [0,1,2],
     [3,4,5],
@@ -14,19 +19,10 @@ const winningPatterns = [
     [2,5,8],
     [0,4,8],
     [2,4,6],
-]
-
-let PlayerO = [];
-let PlayerX = [];
-
-let turnCount = 0
-
-buttonRestart.addEventListener('click', () => {
-    location.reload();
-})
-
+];
 
 const playRound = (e) => {
+    
     //If target items is not empty do nothing
     const gridItem = e.target;
     if(gridItem.innerHTML !== '') return
@@ -35,86 +31,53 @@ const playRound = (e) => {
     let PlayerO_combinations = []
     let PlayerX_combinations = []
 
-    gridItem.innerHTML = currentTurn
-    
+    gridItem.innerHTML = currentTurn;
+    e.target.classList.add("fadeIn");
+
     gridIndex = gridItem.id.slice(gridItem.id.length-1,gridItem.id.length)
     gameboard[gridIndex] = currentTurn
-    
-    if(currentTurn ==='O'){
-        PlayerO.push(gridIndex);
-        if(PlayerO.length>2){
 
-            //Sort contents then run a for loop to to see if the current list contains a winning combination
-            PlayerO.sort();
-            for(let i=0; i <= PlayerO.length -1; i++){
-                for(let j=1; j <= PlayerO.length -1; j++){
-                  for(let k=2; k <= PlayerO.length -1; k++){
-                    if(k>j && j>i){
-                      PlayerO_combinations.push([PlayerO[i]+','+PlayerO[j]+','+PlayerO[k]])
-                    }
-                  }
-                } 
-              }
-              
-            for(let i=0; i <= winningPatterns.length - 1; i++){
-                for(let j=0; j <= PlayerO_combinations.length -1; j++){
-                    if(winningPatterns[i].toString() === (PlayerO_combinations[j].toString())){
-                    winCounterO ++
-                    }
-                }
-            }
+    //Check if there is a winner
+    if(currentTurn ==='O'){
+        if(checkWinner(PlayerO,PlayerO_combinations) > 0){
+            resultID.style.visibility="visible"
+            resultID.classList.add('fadeIn')
+            resultID.innerHTML = 'Player O won the game!';
+            for(i of gameboardItems){
+                i.removeEventListener('click',playRound);
+            };
+            buttonRestart.classList.add('grow-anim');
         }
     } else {
-        PlayerX.push(gridIndex);
-        if(PlayerX.length>2){
-            PlayerX.sort();
-            for(let i=0; i <= PlayerX.length -1; i++){
-                for(let j=1; j <= PlayerX.length -1; j++){
-                  for(let k=2; k <= PlayerX.length -1; k++){
-                    if(k>j && j>i){
-                      PlayerX_combinations.push([PlayerX[i]+','+PlayerX[j]+','+PlayerX[k]])
-                    }
-                  }
-                } 
-              }
-              
-            for(let i=0; i <= winningPatterns.length - 1; i++){
-                for(let j=0; j <= PlayerX_combinations.length -1; j++){
-                    if(winningPatterns[i].toString() === (PlayerX_combinations[j].toString())){
-                    winCounterX ++
-                    }
-                }
-            }
-        };
+        if(checkWinner(PlayerX,PlayerX_combinations) > 0){
+            resultID.style.visibility="visible"
+            resultID.classList.add('fadeIn')
+            resultID.innerHTML = 'Player X won the game!';
+            for(i of gameboardItems){
+                i.removeEventListener('click',playRound);
+            };
+            buttonRestart.classList.add('grow-anim');
+        }
     };
 
-    
-    
-    
-    if(winCounterO > 0 || winCounterX > 0){
-        if(winCounterO > 0){
-            console.log('O has won!')
-            
-        } else if(winCounterX > 0) {
-            console.log('X has won!')
-        }
-        for(i of gameboardItems){
-            i.removeEventListener('click',playRound);
-        };
-
-    }
-
+    //If no one won, then pop up message to restart
     turnCount ++
-    if(turnCount === 9 && (winCounterO === 0 && winCounterX === 0)){
+    if(turnCount === 9 && (winCounter === 0)){
         console.log('No one won the match. Restart the game for a new match')
     }
-    
 }
 
+//Add event listener for each cell in the grid
 gameboardItems.forEach( (items) =>
     items.addEventListener('click', playRound)
 );
 
+//Restart button
+buttonRestart.addEventListener('click', () => {
+    location.reload();
+})
+
+//Match choices
 const matchChoices = () => {
     if (currentTurn === 'X') {
         currentTurn = 'O'
@@ -122,4 +85,32 @@ const matchChoices = () => {
         currentTurn = 'X'
     };
     return currentTurn
+}
+
+//Look for a match in the winning patterns
+const checkWinner = (Player,PlayerCombination) => {
+    //Return index
+    Player.push(gridIndex);
+    if(Player.length>2){
+        //Sort contents then run a for loop to to see if the current list contains a winning combination
+        Player.sort();
+        for(let i=0; i <= Player.length -1; i++){
+            for(let j=1; j <= Player.length -1; j++){
+                for(let k=2; k <= Player.length -1; k++){
+                    if(k>j && j>i){
+                        PlayerCombination.push([Player[i]+','+Player[j]+','+Player[k]])
+                    }
+                }
+            } 
+        }
+        //Check the combination collection if it matches any of the winning patterns
+        for(let i=0; i <= winningPatterns.length - 1; i++){
+            for(let j=0; j <= PlayerCombination.length -1; j++){
+                if(winningPatterns[i].toString() === (PlayerCombination[j].toString())){
+                winCounter++
+                }
+            }
+        }
+    }
+    return winCounter
 }
